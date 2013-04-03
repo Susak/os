eaf56b1e5e67b3ed455971cc7bb2aecfe54c769f
Ruslan <susak-1@yandex.ru>
2013-04-03 16:22:37 +0400

    count simbols

--- dupme/main.c
+++ dupme/main.c
@@ -1,6 +1,6 @@
 #include <unistd.h>
 #include <stdlib.h>
-#include <stdio.h>
+#include <string.h>
 
 int get_size(char *c)
 {
@@ -16,27 +16,38 @@ int get_size(char *c)
 
 void parse_string(size_t size)
 {
+	size_t counter = 0;
+	size_t count_of_write = 0;
+	size_t count_to_read = 0;
 	char *buffer = malloc(size);
+	char *temp_buf = malloc(size);
 	size_t i = 0;
 	size_t read_counter = 1;
 	while (read_counter > 0)
 	{
 		read_counter = read(0, buffer, size);
+		counter = read_counter + count_to_read;
+		count_of_write = 0;
 		i = 0;
 		while ( i < read_counter)
 		{
 			if (buffer[i] == '\n')
 			{
 				write(1, buffer, i + 1);
-				write(1, buffer, i + 1);	
+				write(1, buffer, i + 1);
+				count_of_write = i + 1;	
 			}
 		i++;
 		}
+		count_to_read = counter - count_of_write;
+		memcpy(temp_buf, buffer + count_of_write, count_to_read);
+		buffer = malloc(size);
+		memcpy(buffer, temp_buf, count_to_read);
+		temp_buf = malloc(size);
 	}
 }
 int main(int argc, char *argv[]) {
 	size_t size = get_size(argv[1]);
-	printf("%zu\n", size);
 	parse_string(size + 1);
 	return 0;
 }
