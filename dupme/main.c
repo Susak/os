2a0f60b80d0360b5bb93b9ee6a32aae87598f76a
Ruslan <susak-1@yandex.ru>
2013-04-03 15:51:46 +0400

    get int from argv[1]

--- dupme/main.c
+++ dupme/main.c
@@ -1,6 +1,16 @@
 #include <unistd.h>
 
+int get_size(char *c)
+{
+	int answ = 0;
+	int i = 0;
+	while (c[i] != 0)
+	{
+		answ = answ * 10 + c[i] - '0';
+		i++;
+	}
+}
+
 int main(int argc, char *argv[]) {
-	write(1, argv[1], 3);
 	return 0;
 }
