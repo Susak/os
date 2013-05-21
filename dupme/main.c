ae8890ed40ad89e0871ab39e873a0f60129dd6be
Ruslan <susak-1@yandex.ru>
2013-05-22 01:13:48 +0400

    malloc fix

--- dupme/main.c
+++ dupme/main.c
@@ -9,13 +9,19 @@ typedef int bool;
 void parse_string(size_t size)
 {
     char *buf_new_line = malloc(1);
+    if (buf_new_line == NULL) 
+        _exit(2);
     buf_new_line[0] = '\n';
     bool is_new_string = true;
     size_t counter = 0;
     size_t count_of_write = 0;
     size_t count_to_read = 0;
     char *buffer = malloc(size);
+    if (buffer == NULL)
+        _exit(2);
     char *temp_buf = malloc(size);
+    if (temp_buf == NULL)
+        _exit(2);
     size_t i = 0;
     size_t read_counter = 1;
     while (read_counter > 0)
@@ -45,10 +51,16 @@ void parse_string(size_t size)
             count_to_read = counter - count_of_write;
             memcpy(temp_buf, buffer + count_of_write, count_to_read);
             buffer = malloc(size);
+            if (buffer == NULL)
+                _exit(2);
             memcpy(buffer, temp_buf, count_to_read);
             temp_buf = malloc(size);
+            if (buffer == NULL)
+                _exit(2);
         } else if(counter == size) {
             buffer = malloc(size);
+            if (buffer == NULL)
+                _exit(2);
             is_new_string = false;
         } else {
             count_to_read = counter;
