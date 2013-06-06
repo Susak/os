3e83486ad515b7ab75055430d2d94ec83cdefea0
Ruslan <susak-1@yandex.ru>
2013-06-07 02:46:00 +0400

    client

---
+++ rshell/client.c
@@ -0,0 +1,15 @@
+#include <sys/types.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <unistd.h>
+#include <string.h>
+#include <signal.h>
+#include <sys/socket.h>
+#include <netdb.h>
+#include <pty.h>
+#include <poll.h>
+
+
+int main (int argc, char ** argv) {
+    return 0;
+}
